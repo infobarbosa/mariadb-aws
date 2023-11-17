1a7c2f84d01adb943071d59eb3ae73b3454ec42f
infobarbosa <infobarbosa@yahoo.com.br>
2023-11-16 22:29:19 -0300

    ajustes para legibilidade e melhor tracking

--- scripts/setup_mariadb.sh
+++ scripts/setup_mariadb.sh
@@ -4,48 +4,48 @@ echo "Instalação iniciada em `date -Is`" >> /tmp/install.log
 echo "Instalação iniciada em `date -Is`" >> /tmp/status-instalacao.txt
 sudo apt update -y >> /tmp/install.log
 
-# Pré-requisitos
+echo "`date -Is` - Instalando pré-requisitos" >> /tmp/install.log
 sudo apt update -y >> /tmp/install.log
 sudo apt install -y wget software-properties-common dirmngr ca-certificates apt-transport-https >> /tmp/install.log
 
-# Executando a instalação
+echo "`date -Is` - Executando a instalação" >> /tmp/install.log
 sudo apt install mariadb-server -y >> /tmp/install.log
 
-# Ativando o serviço
+echo "`date -Is` - Ativando o serviço" >> /tmp/install.log
 sudo systemctl enable mariadb >> /tmp/install.log
 
-# Inicializando o serviço
+echo "`date -Is` - Inicializando o serviço" >> /tmp/install.log
 sudo systemctl start mariadb >> /tmp/install.log
 
-# Criando o usuário `barbosa`
+echo "`date -Is` - Criando o usuário barbosa" >> /tmp/install.log
 
 mariadb -e "CREATE USER 'barbosa'@localhost IDENTIFIED BY 'inseguro';" >> /tmp/install.log
 
-# Concedendo acesso administrativo
+echo "`date -Is` - Concedendo acesso administrativo" >> /tmp/install.log
 mariadb -e "GRANT ALL ON *.* TO 'barbosa'@'localhost' IDENTIFIED BY 'inseguro' WITH GRANT OPTION;" >> /tmp/install.log
 
 mariadb -e "FLUSH PRIVILEGES;" >> /tmp/install.log
 
-####################################
-# Instalação do engine ColumnStore #
-####################################
 
-#Baixando o pacote de configuração do repositório do MariaDB
+echo "`date -Is` - Instalação do engine ColumnStore" >> /tmp/install.log
+
+
+echo "`date -Is` - Baixando o pacote de configuração do repositório do MariaDB" >> /tmp/install.log
 wget https://downloads.mariadb.com/MariaDB/mariadb_repo_setup >> /tmp/install.log
 
-# Tornando o pacote executável
+echo "`date -Is` - Tornando o pacote executável" >> /tmp/install.log
 chmod +x mariadb_repo_setup >> /tmp/install.log
 
-# Setup de repositório para a versão `mariadb-10.6`
+echo "`date -Is` - Setup de repositório para a versão mariadb-10.6" >> /tmp/install.log
 sudo ./mariadb_repo_setup --mariadb-server-version="mariadb-10.6" >> /tmp/install.log
 
-# Atualizando a biblioteca de pacotes
+echo "`date -Is` - Atualizando a biblioteca de pacotes" >> /tmp/install.log
 sudo apt update -y >> /tmp/install.log
 
-# Instalando o plugin `mariadb-plugin-columnstore`
+echo "`date -Is` - Instalando o plugin mariadb-plugin-columnstore" >> /tmp/install.log
 sudo apt install -y libjemalloc2 mariadb-backup libmariadb3 mariadb-plugin-columnstore >> /tmp/install.log
 
-### Reiniciando o serviço
+echo "`date -Is` - Reiniciando o serviço" >> /tmp/install.log
 systemctl restart mariadb >> /tmp/install.log
 
 echo "Instalação finalizada em `date -Is`" >> /tmp/install.log
